c2a8e54157013143b9c778843c6ce2369e6e43bb
Lelthar <geraldm1998@hotmail.com>
2017-10-11 05:28:56 -0600

    ventanas limpias
    Se quitaron el texto de los textfiel

--- ProyectoBD1/src/frames/VentanaAgregacion.java
+++ ProyectoBD1/src/frames/VentanaAgregacion.java
@@ -25,7 +25,7 @@ public class VentanaAgregacion extends javax.swing.JFrame {
     public VentanaAgregacion(OperacionesAlgebraRelacional pVentanaOperaciones) {
         initComponents();
         this.ventanaOperaciones = pVentanaOperaciones;
-        this.setTitle("Realización de Intersección");
+        this.setTitle("Realización de Agregacion");
         this.setLocationRelativeTo(null);
         this.setResizable(false);
     }
--- ProyectoBD1/src/frames/VentanaDiferenciaConjuntos.java
+++ ProyectoBD1/src/frames/VentanaDiferenciaConjuntos.java
@@ -25,7 +25,7 @@ public class VentanaDiferenciaConjuntos extends javax.swing.JFrame {
     public VentanaDiferenciaConjuntos(OperacionesAlgebraRelacional pVentanaOperaciones) {
         initComponents();
         this.ventanaOperaciones = pVentanaOperaciones;
-        this.setTitle("Realización de Intersección");
+        this.setTitle("Realización de Diferencia de Conjuntos");
         this.setLocationRelativeTo(null);
         this.setResizable(false);
     }
